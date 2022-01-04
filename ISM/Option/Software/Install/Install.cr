95684723490c8f6f1cee804d55017d5934ea3e4b
Fulgurance <zohran.londais@gmail.com>
2022-01-04 12:35:08 +0100

    Patched wrong path

--- ISM/Option/Software/Install/Install.cr
+++ ISM/Option/Software/Install/Install.cr
@@ -243,7 +243,7 @@ module ISM
                             matchingSoftwaresArray.each do |software|
 
                                 file = File.open("ISM.task", "w")
-                                file << "require \"./#{software.port + ISM::Default::Path::SoftwaresDirectory + software.name + "/" + software.version + "/" + software.version + ".cr"}\"\n"
+                                file << "require \"./#{software.port + "/"  + ISM::Default::Path::SoftwaresDirectory + software.name + "/" + software.version + "/" + software.version + ".cr"}\"\n"
                                 file << "target = Target.new\n"
                                 file << "target.download\n"
                                 file << "target.check\n"
