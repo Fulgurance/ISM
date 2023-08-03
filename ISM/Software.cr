1526a7c91e94b8c5c140d6724738fd6655641b20
Fulgurance <zohran.londais@gmail.com>
2023-08-03 19:36:05 +0100

    Patched missing dup instruction and added proper filter

--- ISM/Software.cr
+++ ISM/Software.cr
@@ -1088,10 +1088,10 @@ module ISM
 
         def getFullKernelKconfigFile(kconfigPath : String) : Array(String)
             content = File.read_lines(kernelKconfigFilePath)
-            content = content.map { |line| line.gsub(/\s+/, "") }
+            content = content.map { |line| line.gsub(/(?!\s)\s+/, "") }
 
-            result = content
-            nextResult = result
+            result = content.dup
+            nextResult = result.dup
 
             loop do
 
@@ -1115,7 +1115,7 @@ module ISM
 
                         begin
                             temp = File.read_lines(path)
-                            temp = temp.map { |line| line.gsub(/\s+/, "") }
+                            temp = temp.map { |line| line.gsub(/(?!\s)\s+/, "") }
 
                             nextResult += temp
                         rescue
