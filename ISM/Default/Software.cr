e8c980c4278ba2e8c7070b81cab60a47f016ea1d
Fulgurance <zohran.londais@gmail.com>
2024-02-10 17:16:11 +0000

    Updated kconfig keywords regex table

--- ISM/Default/Software.cr
+++ ISM/Default/Software.cr
@@ -19,15 +19,15 @@ module ISM
             DownloadSourceRedirectionErrorText1 = "Got status "
             DownloadSourceRedirectionErrorText2 = " but no location was sent"
             DownloadSourceCodeErrorText = "Received HTTP status code "
-            KconfigKeywords = { :config => /[\t\s]+config[\t\s]+/,
-                                :bool => /[\t\s]+bool[\t\s]+/,
-                                :dependsOn => /[\t\s]+depends[\t\s]+on[\t\s]+/,
-                                :endif => /[\t\s]+endif[\t\s]+/,
-                                :if => /[\t\s]+if[\t\s]+/,
-                                :menuconfig => /[\t\s]+menuconfig[\t\s]+/,
-                                :tristate => /[\t\s]+tristate[\t\s]+/,
-                                :select => /[\t\s]+select[\t\s]+/,
-                                :source => /[\t\s]+source[\t\s]+/}
+            KconfigKeywords = { :config => /[\s\]+config[\s]+/,
+                                :bool => /[\s\]+bool[\s]+/,
+                                :dependsOn => /[\s\]+depends[\s]+on[\s]+/,
+                                :endif => /[\s\]+endif[\s]+/,
+                                :if => /[\s\]+if[\s]+/,
+                                :menuconfig => /[\s\]+menuconfig[\s]+/,
+                                :tristate => /[\s\]+tristate[\s]+/,
+                                :select => /[\s\]+select[\s]+/,
+                                :source => /[\s\]+source[\s]+/}
 
         end
 
