ade0288bf765893ff770ee58b670e77ae4004732
Fulgurance <zohran.londais@gmail.com>
2024-05-17 23:42:32 +0100

    Patched wrong variable type

--- ISM/Software.cr
+++ ISM/Software.cr
@@ -1236,7 +1236,7 @@ module ISM
             filesList = Dir.glob(["#{builtSoftwareDirectoryPath(false)}/**/*"], match: :dot_files)
             installedFiles = Array(String).new
 
-            directoryNumber, symlinkNumber, fileNumber, totalSize = Uint128.new(0)
+            directoryNumber, symlinkNumber, fileNumber, totalSize = UInt128.new(0)
 
             filesList.each do |entry|
 
