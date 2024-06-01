035c1722edfc489d32e69a39a94ecd20080109a0
Fulgurance <zohran.londais@gmail.com>
2024-06-01 14:33:37 +0100

    Added experimental patch

--- ISM/Software.cr
+++ ISM/Software.cr
@@ -1167,7 +1167,7 @@ module ISM
         end
 
         def configureSource(arguments = Array(String).new, path = String.new, configureDirectory = String.new, environment = Hash(String, String).new)
-            if @buildDirectory && (configureDirectory != "" || path.squeeze("/") != mainWorkDirectoryPath.squeeze("/"))
+            if @buildDirectory && (configureDirectory != "" && path.squeeze("/") != mainWorkDirectoryPath.squeeze("/"))
                 configureCommand = "../#{configureDirectory}/configure"
             else
                 configureCommand = "./#{configureDirectory}/configure"
