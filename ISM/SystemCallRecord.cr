bd345efa1c7ee5f730215401101418793d7c4ad0
Fulgurance <zohran.londais@gmail.com>
2024-06-15 20:02:14 +0100

    Added exit code support

--- ISM/SystemCallRecord.cr
+++ ISM/SystemCallRecord.cr
@@ -2,21 +2,24 @@ module ISM
 
     class SystemCallRecord
 
+        property exitCode :     Int32
         property command :      String
         property path :         String
         property environment :  Hash(String,String)
 
-        def initialize( @command =      String.new,
+        def initialize( @exitCode =     Int32.new,
+                        @command =      String.new,
                         @path =         String.new,
                         @environment =  Hash(String,String).new)
         end
 
         def formattedOutput
-            command =       "#{ISM::Default::SystemCallRecord::FormattedOutputText1}#{@command}"
-            path =          "#{ISM::Default::SystemCallRecord::FormattedOutputText2}#{@path}"
-            environment =   "#{ISM::Default::SystemCallRecord::FormattedOutputText3}#{(@environment.map { |key| key.join("=") }).join(" ")}"
+            code =          "#{ISM::Default::SystemCallRecord::FormattedOutputText1}#{@exitCode.to_s}\n"
+            command =       "#{ISM::Default::SystemCallRecord::FormattedOutputText2}#{@command}"
+            path =          "#{ISM::Default::SystemCallRecord::FormattedOutputText3}#{@path}"
+            environment =   "#{ISM::Default::SystemCallRecord::FormattedOutputText4}#{(@environment.map { |key| key.join("=") }).join(" ")}"
 
-            return "#{command}#{path}#{environment}"
+            return "#{exitCode}#{command}#{path}#{environment}"
         end
 
     end
