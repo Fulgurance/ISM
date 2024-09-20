1a3af85b68167e8271107b735c088d7474470909
Fulgurance <zohran.londais@gmail.com>
2024-09-20 17:45:18 +0100

    Temporary code for experimental patch

--- ISM/CommandLine.cr
+++ ISM/CommandLine.cr
@@ -1526,17 +1526,13 @@ module ISM
             processOutput = IO::Memory.new
             processResult = IO::Memory.new
 
-            loadingOutput = IO::MultiWriter.new(STDOUT,processOutput)
-
             process = Process.run(  "crystal build --release --progress #{ISM::Default::Filename::Task}.cr -o #{@settings.rootPath}#{ISM::Default::Path::RuntimeDataDirectory}#{ISM::Default::Filename::Task} -f json",
-                                    output: loadingOutput,
+                                    output: processOutput,
                                     error: processResult,
                                     shell: true,
                                     chdir: "#{@settings.rootPath}#{ISM::Default::Path::RuntimeDataDirectory}")
 
-            processOutput.to_s.each do |output|
-                print output
-            end
+            print processOutput
 
             processResult.rewind
 
