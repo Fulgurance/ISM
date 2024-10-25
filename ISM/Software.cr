35a76c8483c36e2ab764f813b4e246bc64652439
Fulgurance <zohran.londais@gmail.com>
2024-10-25 22:07:26 +0100

    Added missing instruction for chroot

--- ISM/Software.cr
+++ ISM/Software.cr
@@ -468,7 +468,7 @@ module ISM
             if Ism.settings.installByChroot
                 chrootCommand = <<-CODE
                 #!/bin/bash
-                cd #{path} && #{environmentCommand} #{command}
+                source /etc/profile && cd #{path} && #{environmentCommand} #{command}
                 CODE
 
                 process = runChrootTasks(chrootCommand)
