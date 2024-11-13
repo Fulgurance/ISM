1efca80aedb327802e285a7722c446e2289d1ac6
Fulgurance <zohran.londais@gmail.com>
2024-11-13 08:14:43 +0000

    Patched non complete command (need to test if needed to squeeze / character

--- ISM/PythonPackageSoftware.cr
+++ ISM/PythonPackageSoftware.cr
@@ -7,14 +7,15 @@ module ISM
 
             runPipCommand(  arguments: "install --no-dependencies --target \"#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}\" '#{@information.name}==#{@information.version}'")
 
-            packagesPath = "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/lib/python#{softwareMajorVersion}.#{softwareMinorVersion}/site-packages"
+            packagesPath = "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}usr/lib/python#{softwareMajorVersion}.#{softwareMinorVersion}/site-packages"
 
             makeDirectory(packagesPath)
 
             Dir.glob(["#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/**/*"], match: :dot_files).each do |filePath|
 
                 if filePath != packagesPath && filePath != "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}bin"
-                    moveFile(,"#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/lib/python#{softwareMajorVersion}.#{softwareMinorVersion}/site-packages/*")
+                    moveFile(   path:       filePath,
+                                newPath:    "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/lib/python#{softwareMajorVersion}.#{softwareMinorVersion}/site-packages/")
                 end
 
             end
