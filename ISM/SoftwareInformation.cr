9039ca2147d40f558fd960ef9ab62e43c7449e85
Fulgurance <zohran.londais@gmail.com>
2025-01-26 14:35:41 +0000

    Patched wrong condtion

--- ISM/SoftwareInformation.cr
+++ ISM/SoftwareInformation.cr
@@ -354,7 +354,7 @@ module ISM
             return @dependencies.reject { |entry| dependencyIsUnique(entry.fullName) && !uniqueDependencyIsEnabled(entry.fullName)} + dependenciesArray
         else
             #REJECT INSTALLED DEPENDENCIES AND UNIQUE DEPENDENCIES NOT SELECTIONED
-            return @dependencies.reject { |entry| Ism.softwareIsInstalled(entry.information) || dependencyIsUnique(entry.fullName) && !uniqueDependencyIsEnabled(entry.fullName)} + dependenciesArray.reject { |entry| entry.passEnabled && !Ism.systemInformation.crossToolchainFullyBuilt}
+            return @dependencies.reject { |entry| Ism.softwareIsInstalled(entry.information) || dependencyIsUnique(entry.fullName) && !uniqueDependencyIsEnabled(entry.fullName)} + dependenciesArray.reject { |entry| entry.passEnabled && Ism.systemInformation.crossToolchainFullyBuilt}
         end
 
         rescue error
