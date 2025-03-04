49a1bb17532ceedd388ef4248166ae5740c69eae
Fulgurance <zohran.londais@gmail.com>
2025-03-04 13:24:16 +0000

    Added back system feature

---
+++ ISM/Default/Option/System/System.cr
@@ -0,0 +1,20 @@
+module ISM
+
+    module Default
+
+        module Option
+
+            module System
+
+                ShortText = "-sy"
+                LongText = "system"
+                Description = "Manage the system"
+                Options = Array(ISM::CommandLineOption).new
+
+            end
+
+        end
+
+    end
+
+end
