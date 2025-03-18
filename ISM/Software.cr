424d3fce1c4b7ef6ffc6db1b71b75c4f5712813f
Fulgurance <zohran.londais@gmail.com>
2025-03-18 14:36:14 +0000

    Added experimental patch for modes

--- ISM/Software.cr
+++ ISM/Software.cr
@@ -260,7 +260,7 @@ module ISM
 
         #Special function to improve performance (Internal use only)
         def changeFileModeNoChroot(path : String, mode : String)
-            File.chmod(path, mode.to_i(base: 8))
+            File.chmod(path, mode.to_i)
 
             rescue error
                 Ism.printSystemCallErrorNotification(error)
