521cefcd3687a0655039c44d4a3768a71cbb6f2b
Fulgurance <zohran.londais@gmail.com>
2025-03-24 12:41:03 +0000

    Improved libc implementation

--- ISM/Libc.cr
+++ ISM/Libc.cr
@@ -1,6 +1,6 @@
 #Libc functions to deal with SUID and SGID bit
 lib LibC
     fun getgid : Int
-    fun seteuid(gid : UidT) : Int
+    fun seteuid(uid : UidT) : Int
     fun setegid(gid : UidT) : Int
 end
