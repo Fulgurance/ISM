eed6b0ec22410d0dbcf71c463fa8d0af11a53463
Fulgurance <zohran.londais@gmail.com>
2025-03-31 22:48:42 +0100

    Removed useless accessor

--- Main.cr
+++ Main.cr
@@ -10,7 +10,7 @@ begin
     egidResult = LibC.setegid(ISM::Default::CommandLine::SystemId.to_i)
 
     #We check if the program have the suid bit set
-    if LibC.euidResult.negative? || LibC.egidResult.negative?
+    if euidResult.negative? || egidResult.negative?
         #printNeedSuidBitNotification
         puts "Need setted bit"
     else
