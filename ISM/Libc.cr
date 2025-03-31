836839db2550e830177ec98c0b007172e6fbf8ba
Fulgurance <zohran.londais@gmail.com>
2025-03-31 05:55:30 +0100

    Added missing functions

--- ISM/Libc.cr
+++ ISM/Libc.cr
@@ -2,6 +2,8 @@
 lib LibC
     fun seteuid(uid : UidT): Int
     fun setegid(uid : UidT): Int
+    fun setgid(uid : UidT): Int
+    fun getgid : UidT
     fun getegid : UidT
     fun geteuid : UidT
 end
