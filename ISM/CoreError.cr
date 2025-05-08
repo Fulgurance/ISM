54d78ace34d6c780c09f2a3fdda60117a44bc51a
Fulgurance <zohran.londais@gmail.com>
2025-05-08 17:50:06 +0100

    Started to integrate core parts. Need tests first

---
+++ ISM/CoreError.cr
@@ -0,0 +1,56 @@
+module ISM
+
+    module Core
+
+        module Error
+
+            module Default
+                Title = "Internal error"
+                InstallerTitle = "Installer failure"
+                Help = "#{CommandLine::Name.upcase} raised that error because the ran script did not call properly a system command or the system command itself need to be fix."
+                SystemCommandFailure = "The following system command failed to run: "
+            end
+
+            def self.show(  className : String,
+                            functionName : String,
+                            errorTitle : String,
+                            error : String,
+                            exception = Exception.new,
+                            information = String.new,
+                            errorCode = 1)
+
+                fullLog = (exception.backtrace.empty? ? exception.backtrace.join("\n") : exception.message)
+
+                title = (className == "Software" ? Default::InstallerTitle.colorize(:red) : Default::Title.colorize(:red))
+
+                errorText = "#{fullLog.colorize(:red)}"
+                help = "#{Default::Help.colorize(:red)}"
+
+                errorReport = <<-REPORT
+                [ #{title} ]
+
+                Class: #{className.colorize(:red)}
+                Function: #{functionName.colorize(:red)}
+
+                #{errorTitle.colorize(:red)}
+                #{error}
+
+                Exception:
+                #{errorText}
+
+                Exit code: #{errorCode.colorize(:red)}
+
+                #{help.colorize(:red)}
+                REPORT
+
+                puts "\n#{errorReport}\n"
+
+                ISM::Core.exitProgram(code: errorCode)
+
+            end
+
+        end
+
+    end
+
+end
