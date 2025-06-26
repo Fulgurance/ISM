dc208a2f4e6b6aca25f6aba27cb28a377a737785
Fulgurance <zohran.londais@gmail.com>
2025-06-26 18:43:16 +0100

    Added missing extension

--- ISM/Software.cr
+++ ISM/Software.cr
@@ -748,7 +748,7 @@ module ISM
 
                 checkAuthenticity(  archive:            "#{workDirectoryPathNoChroot}/#{ISM::Default::Software::SourcesArchiveName}",
                                     archiveSignature:   "#{workDirectoryPathNoChroot}/#{ISM::Default::Software::SourcesSignatureArchiveName}",
-                                    publicSignature:    "#{workDirectoryPathNoChroot}/#{fileName}")
+                                    publicSignature:    "#{workDirectoryPathNoChroot}/#{fileName}#{PublicKeyExtensionName}")
             end
 
             rescue exception
