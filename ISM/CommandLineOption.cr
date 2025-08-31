a1165566fc0b10f9aaf48ae3170fb2359bb74041
Fulgurance <zohran.londais@gmail.com>
2025-08-31 18:35:01 +0100

    Experimental update to align properly text

--- ISM/CommandLineOption.cr
+++ ISM/CommandLineOption.cr
@@ -15,11 +15,53 @@ module ISM
 
         def showHelp
             puts @description
-            @options.each do |argument|
-                puts    "\t" + "#{argument.shortText.colorize(:white)}" +
-                        "\t" + "#{argument.longText.colorize(:white)}" +
-                        "\t" + "#{argument.description.colorize(:green)}"
+
+            shortTextArray = @options.map { |entry| entry.shortText}
+            longTextArray = @options.map { |entry| entry.longText}
+
+            highestShortTextSize = shortTextArray.max_by { |entry| entry.size }
+            highestLongTextSize = longTextArray.max_by { |entry| entry.size }
+
+            @options.each do |option|
+
+                shortTextPadding = String.new
+
+                if option.shortText.size < 8
+                    shortTextPadding += "\t"
+                end
+
+                limit = (highestShortTextSize.size/8 - option.shortText.size/8)
+
+                (0..(limit)).each do |i|
+                    shortTextPadding += "\t"
+                end
+
+                ############################
+
+                longTextPadding = String.new
+
+                if option.shortText.size < 8
+                    longTextPadding += "\t"
+                end
+
+                limit = (highestLongTextSize.size/8 - option.longText.size/8)
+
+                (0..(limit)).each do |i|
+                    longTextPadding += "\t"
+                end
+
+                #############################
+
+                puts    "\t#{option.shortText.colorize(:white)}#{shortTextPadding}" +
+                        "#{option.longText.colorize(:white)}#{longTextPadding}" +
+                        "#{option.description.colorize(:green)}"
             end
+
+            # @options.each do |argument|
+            #     puts    "\t" + "#{argument.shortText.colorize(:white)}" +
+            #             "\t" + "#{argument.longText.colorize(:white)}" +
+            #             "\t" + "#{argument.description.colorize(:green)}"
+            # end
 
             rescue exception
                 ISM::Error.show(className: "CommandLineOption",
