e473946f9e5afb537dce9248b3fa0a4cb4b6191a
Fulgurance <zohran.londais@gmail.com>
2025-09-13 15:11:46 +0100

    Patched bug when a minor error occur

--- ISM/CommandLine/CommandLine.cr
+++ ISM/CommandLine/CommandLine.cr
@@ -2491,7 +2491,7 @@ module ISM
 
             processResult.rewind
 
-            if processResult.to_s != ""
+            if processResult.to_s != "" && processResult.starts_with?("[")
                 taskError = Array(ISM::TaskBuildingProcessError).from_json(processResult.to_s.gsub("\"size\":null","\"size\":0"))[-1]
 
                 showTaskCompilationFailedMessage
